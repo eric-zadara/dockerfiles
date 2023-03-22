9d921f9485744db681f1b61eec0333cc53f57c02
eric-zadara <eric@zadarastorage.com>
2023-03-22 15:40:08 -0700

    debug: remove -t from docker run commands

--- ansible_ansible-builder/version.sh
+++ ansible_ansible-builder/version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 IMAGE=$(awk '/^FROM/{a=$NF} END{print a}' Dockerfile)
 docker pull --quiet "${IMAGE}" > /dev/null
-docker run --rm -t "${IMAGE}" ansible-builder --version
+docker run --rm "${IMAGE}" python3 -u /usr/local/bin/ansible-builder --version 2>/dev/null
