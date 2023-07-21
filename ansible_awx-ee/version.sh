c209bd091b108054f84636ca9d7e1e7e62084088
eric-zadara <eric@zadarastorage.com>
2023-07-21 09:40:22 -0700

    revise version check to always lookup latest

--- ansible_awx-ee/version.sh
+++ ansible_awx-ee/version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-IMAGE=$(awk '/^FROM/{a=$NF} END{print a}' Dockerfile)
+IMAGE=$(awk '/^FROM/{a=$NF} END{print a}' Dockerfile | sed 's@${CONTAINER_VERSION}@latest@g')
 docker pull --quiet "${IMAGE}" > /dev/null
 docker run --rm "${IMAGE}" awx --version
--- ansible_awx/version.sh
+++ ansible_awx/version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-IMAGE=$(awk '/^FROM/{a=$NF} END{print a}' Dockerfile)
+IMAGE=$(awk '/^FROM/{a=$NF} END{print a}' Dockerfile | sed 's@${CONTAINER_VERSION}@latest@g')
 docker pull --quiet "${IMAGE}" > /dev/null
 docker run --rm "${IMAGE}" bash -c 'source /var/lib/awx/venv/awx/bin/activate && pip list 2>&- | awk "/^awx/{print \$NF}"'
