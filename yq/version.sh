11ef472714c5223bd81926b51db00c008e836713
eric-zadara <eric@zadarastorage.com>
2023-07-21 09:26:56 -0700

    chore: cleanup and prevent nulls in the versions scripts

--- yq/version.sh
+++ yq/version.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 GITHUB_REPO="mikefarah/yq"
-curl -sL "https://api.github.com/repos/${GITHUB_REPO}/releases/latest" | jq -r ".tag_name" | sed 's@^v@@g'
+RESULT=$(curl -sL "https://api.github.com/repos/${GITHUB_REPO}/releases/latest" | jq -r ".tag_name" | sed 's@^v@@g')
+if [[ -n "${RESULT}" && "${RESULT}" != "null" ]]; then
+	echo "${RESULT}"
+else
+	exit 1
+fi
