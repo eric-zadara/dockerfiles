461cfae46f1e1652919949a0eb938c1cabd50f41
eric-zadara <eric-zadara@users.noreply.github.com>
2025-07-14 11:52:55 -0700

    python2: ignore apt output for getting python version

--- python2/version.sh
+++ python2/version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOCKER_IMAGE=$(awk '/^FROM/{a=$NF} END{print a}' Dockerfile)
 docker pull --quiet "${DOCKER_IMAGE}" > /dev/null
-PY_VERSION=$(docker run --entrypoint /bin/sh --rm "${DOCKER_IMAGE}" -c "apt-get update -y && apt-get install -f -y python2.7 && python2.7 --version | awk '{print \$NF}'")
+PY_VERSION=$(docker run --entrypoint /bin/sh --rm "${DOCKER_IMAGE}" -c "apt-get update -y > /dev/null 2>&1 && apt-get install -f -y python2.7 > /dev/null 2>&1 && python2.7 --version | awk '{print \$NF}'")
 [[ -z "${PY_VERSION}" ]] && exit 1
 echo "${PY_VERSION}"
