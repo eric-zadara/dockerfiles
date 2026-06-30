8c4ce6707177c8119e798aa10a76c2b312f92d1e
eric-zadara <eric-zadara@users.noreply.github.com>
2026-06-30 11:09:07 -0700

    awx-ee: fix exit signal

--- ansible_awx-ee/version.sh
+++ ansible_awx-ee/version.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 IMAGE=$(awk '/^FROM/{a=$NF} END{print a}' Dockerfile | sed 's@${CONTAINER_VERSION}@latest@g')
 docker pull --quiet "${IMAGE}" > /dev/null
-docker run --rm "${IMAGE}" awx --version | grep -v -e 'dev'
+
+RESULT=$(docker run --rm "${IMAGE}" awx --version)
+[[ "${RESULT}" != *"dev"* ]] && echo ${RESULT} || true
